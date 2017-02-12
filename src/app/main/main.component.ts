c0b90d71e9eea8293381d75f6186c67be9918b63
Mikhail <mikhail.mikhalev37@gmail.com>
2017-02-12 19:20:13 +0300

    Added left panel on chat window

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const avatar = './assets/images/avatar.jpg';
+
 @Component({
   selector: 'chat-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  public currentId = 111;
+  
+  public friends = [
+      { id: 1, name: 'Ivan', surname: 'Ivanov', avatar, messages: [ { from: 1, text: 'Hi man!' } ] },
+      { id: 2, name: 'Petr', surname: 'Petrov', avatar, messages: [ { from: 2, text: 'Hi man!' } ]  },
+      { id: 3, name: 'Mikhail', surname: 'Semenov', avatar, messages: [ { from: 3, text: 'Hi man!' } ]  },
+      { id: 4, name: 'Alexei', surname: 'Legkov', avatar, messages: [ { from: 4, text: 'Hi man!' } ]  },
+      { id: 5, name: 'Denis', surname: 'Maksimov', avatar, messages: [ { from: 5, text: 'Hi man!' } ]  }
+    ];
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
